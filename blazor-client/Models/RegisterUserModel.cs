50a8726497ee38aa01a71455ecbb96a8761c4883
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-03 10:57:03 +0300

    feat: add register page

---
+++ blazor-client/Models/RegisterUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_client.Models 
+{
+    public class RegisterUserModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Минимальная длина логина 3 символа")]
+        public string login { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Минимальная длина пароля 6 символов")]
+        public string password { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+}
