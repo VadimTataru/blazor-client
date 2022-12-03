1ca90a638b83eee64068a92c43396dbc62419549
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-03 11:05:37 +0300

    feat: add login page

---
+++ blazor-client/Models/LoginUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_client.Models
+{
+    public class LoginUserModel
+    {
+        [Required]
+        public string login { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
